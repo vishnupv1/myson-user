3a73a6c52e64e2d4edfe30f480a4b77cec3efcb1
vishnupv1 <vishnupv0808@gmail.com>
2024-10-21 21:25:34 +0530

    video

--- src/Components/BannerSlider.js
+++ src/Components/BannerSlider.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import video  from "../videos/video.mp4";
+const BannerVideo = () => {
+  const videoRef = useRef(null);
 
-const BannerSlider = () => {
+  useEffect(() => {
+    const videoElement = videoRef.current;
+
+    // Start video at 1:06 (66 seconds)
+    videoElement.currentTime = 66;
+
+    // Automatically stop video at 3:05 (185 seconds)
+    const stopVideoAtEnd = () => {
+      if (videoElement.currentTime >= 185) {
+        videoElement.pause();
+      }
+    };
+
+    videoElement.addEventListener("timeupdate", stopVideoAtEnd);
+
+    // Clean up the event listener when the component is unmounted
+    return () => {
+      videoElement.removeEventListener("timeupdate", stopVideoAtEnd);
+    };
+  }, []);
+
   return (
     <div className="banner-container">
-      <iframe
-        width="100%"
-        height="500px"
-        src="https://www.youtube.com/embed/v8lwDWJc-CQ?si=t50q7Assu9u5HNGs&amp;controls=0&amp;start=66"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="autoplay"
-        allowFullScreen
-      ></iframe>
+      <video ref={videoRef} width="100%" height="500px" controls autoPlay muted>
+        <source src={video} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 };
 
-export default BannerSlider;
+export default BannerVideo;
