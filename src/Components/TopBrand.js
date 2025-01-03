901dde80096009d6d7184ac9fcd5ef281d79dc47
vishnu <vishnu@nhrtechnologies.com>
2025-01-03 19:49:20 +0530

    crem logo

--- src/Components/TopBrand.js
+++ src/Components/TopBrand.js
@@ -16,7 +16,7 @@ const TopBrands = () => {
     "https://www.pi-india.com/uploaded_files/cf66d20b8daffd.jpg",
     "https://media.licdn.com/dms/image/v2/D4D0BAQGByKydIdtFMw/company-logo_200_200/company-logo_200_200/0/1694007804218?e=1744243200&v=beta&t=3tkU5CbfvkTowK6YTDJCpvIzQt9fYT7QW6c2iEy-81A",
     "https://media.licdn.com/dms/image/v2/C560BAQH8UuwERcv21Q/company-logo_200_200/company-logo_200_200/0/1630567287582/hoshizaki_america_logo?e=1744243200&v=beta&t=knzCSJXLZ5wAALXTruWxZucgrMPLC5mWF_HuDvCGHME",
-    "https://info.welbilt.com/hs-fs/hubfs/RGB_CRE_Grey-1.png?width=254&height=77&name=RGB_CRE_Grey-1.png",
+    "https://scontent.fhyd7-1.fna.fbcdn.net/v/t39.30808-6/329119842_743516480334935_7746442451502530939_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=Xj4GUPbixdEQ7kNvgHktlZB&_nc_zt=23&_nc_ht=scontent.fhyd7-1.fna&_nc_gid=AD9tCh3aPYxGPcDT9y7XHax&oh=00_AYBFbmrBUKXUSHNrfn9MVXhc7f9T4S_WPiwEMmPuVlZdrA&oe=677DD956",
     "https://www.convotherm.com/images/shared/logos/Convotherm_Color.svg",
     "https://www.lincolnfp.com/images/shared/logos/Lincoln_Black.svg",
     "https://www.merrychef.com/images/shared/logos/Merrychef_PMS186C.svg",
