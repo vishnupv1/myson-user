0f534d3ebc3a21118e7e87e4cfdbaf179b87a0dc
vishnu <vishnu@nhrtechnologies.com>
2025-01-16 01:08:17 +0530

    customer name

--- src/Components/TestimonialComponent.js
+++ src/Components/TestimonialComponent.js
@@ -1,33 +1,39 @@
-import React from 'react';
-import './Testimonial.css'; // Import the CSS file
+import React from "react";
+import "./Testimonial.css"; // Import the CSS file
 
 const Testimonial = () => {
   return (
-    <div className='testimonial'>
-      <h4 style={{ fontWeight: 'bold', textAlign: 'center' }} className='title'>Our Customers Love Us!</h4>
-      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+    <div className="testimonial">
+      <h4 style={{ fontWeight: "bold", textAlign: "center" }} className="title">
+        Our Customers Love Us!
+      </h4>
+      <div
+        style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+      >
         <div className="testimonial-container">
           <div className="testimonial-card">
-            <div className="testimonial-image">
+            {/* <div className="testimonial-image">
               <h3>Mr. ALAN</h3>
-            </div>
+            </div> */}
             <div className="testimonial-content">
               <p className="testimonial-text">
-                "This service has been fantastic! The team was professional, and the results exceeded our expectations."
+                "This service has been fantastic! The team was professional, and
+                the results exceeded our expectations."
               </p>
-              <p className="testimonial-author">- John Doe</p>
+              <p className="testimonial-author">- Toms</p>
             </div>
           </div>
         </div>
 
         <div className="testimonial-container">
           <div className="testimonial-card">
-            <div className="testimonial-image">
+            {/* <div className="testimonial-image">
               <h3>Mrs. JULIET</h3>
-            </div>
+            </div> */}
             <div className="testimonial-content">
               <p className="testimonial-text">
-                "This service has been fantastic! The team was professional, and the results exceeded our expectations."
+                "This service has been fantastic! The team was professional, and
+                the results exceeded our expectations."
               </p>
               <p className="testimonial-author">- Jane Smith</p>
             </div>
@@ -36,12 +42,13 @@ const Testimonial = () => {
 
         <div className="testimonial-container">
           <div className="testimonial-card">
-            <div className="testimonial-image">
+            {/* <div className="testimonial-image">
               <h3>Mr. FELIX</h3>
-            </div>
+            </div> */}
             <div className="testimonial-content">
               <p className="testimonial-text">
-                "This service has been fantastic! The team was professional, and the results exceeded our expectations."
+                "This service has been fantastic! The team was professional, and
+                the results exceeded our expectations."
               </p>
               <p className="testimonial-author">- Alice Johnson</p>
             </div>
